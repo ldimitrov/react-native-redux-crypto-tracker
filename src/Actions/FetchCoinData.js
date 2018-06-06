8342e46bdba9155b8b7a76e4b43cd9990628edbe
ldimitrov <ldimitrov070@gmail.com>
2018-06-06 17:32:45 +0200

    implemented reduxt thunk function for fetching data

---
+++ src/Actions/FetchCoinData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { apiBaseUrl } from './../Utils/Constants';
+import {
+    FETCHING_COIN_DATA,
+    FETCHING_COIN_DATA_SUCCESS,
+    FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
+
+export default function FetchCoinData() {
+    return dispatch => {
+        dispatch({ type: FETCHING_COIN_DATA })
+
+        return axios.get(`${apiBaseUrl}/v1/ticker/?limit=10`)
+            .then(response => {
+                dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: response.data })
+            })
+            .catch(error => {
+                dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: error.data })
+            })
+
+    }
+}
